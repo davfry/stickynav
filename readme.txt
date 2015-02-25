08bb65caa2e96681c3e318c0267c0900bac75caf
davfry <davecfryer@gmail.com>
2015-02-24 18:22:02 -0700

    first commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+
+license MIT
+
+js function that will keep the navigation at the top of a page.
---
+++ stickynav.js
@@ -0,0 +1,15 @@
+var initialOffset = false;
+
+function () {
+  var nav = document.querySelector('nav');
+  if (!nav) return;
+  initialOffset = initialOffset || nav.offsetTop;
+  if (initialOffset - document.body.scrollTop >= 0){
+    nav.style.position = 'initial';
+    nav.nextSibling.style.marginTop = '0px';
+  } else {
+    nav.style.position = 'fixed';
+    nav.style.top = 0;
+    nav.nextSibling.style.marginTop = nav.offsetHeight +'px';
+  }
+};
